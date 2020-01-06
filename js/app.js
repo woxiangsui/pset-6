36e46d4f262fc6a9330a9daf6586a0c281cb4e8e
unknown <jlin@ucvts.org>
2020-01-05 20:50:19 -0500

    start over?

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
-const list = document.getElementById("list"); //elements
-const input = document.getElementById("input");
-
-const CHECK = "fa-check-circle-o"; //class
-const NOCHECK = "fa-check-circle-o";
-//const STRIKETHROUGH = text.strike();
-
-function addItem(todo) {
-  const item = "";
-  const position =
+window.onload = function() {
+  document.getElementById("enter").onclick = addItem;
 }
+
+const addItem = function() {
+  var list = document.createElement("li");
+  var valueInput = document.getElementById("input").value;
+  var input = document.getElementById("input");
+  var temp = document.createTextNode(valueInput);
+  list.append(temp);
+
+  /* if (input) {
+    input.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        addItem();
+      }
+    });
+  }
+  */
+
+    if (valueInput !== "" ) {
+      document.getElementById("list").append(list);
+    }
+    document.getElementById("input").value = "";
+
+    var span = document.createElement("SPAN");
+    var text = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.append(text);
+    list.append(span);
+    text = input.value.trim()
+
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
