abb692b0f9eadd94440eaa0f9e6b046ed9f58210
unknown <jlin@ucvts.org>
2020-01-06 22:35:38 -0500

    ???

--- js/app.js
+++ js/app.js
@@ -14,6 +14,23 @@ function addItems() {
   }
 }
 
+function display() {
+  var todo = addItems();
+
+  var html = "<ul>";
+  for (var i = 0; i < todo.length; i++) {
+    html += "<li>" + todo[i] + '<button class="remove" id="' + i + '">x</button></li>';
+  };
+  html += "</ul>";
+
+  document.getElementById("list").innerHTML = html;
+
+  var buttons = document.getElementsByClassName("remove");
+  for (var i = 0; i < buttons.length; i++) {
+    button[i].addEventListener('click', remove);
+  };
+}
+
 function enterKey(event) {
   if (event.keyCode == 13) {
     event.preventDefault();
@@ -31,69 +48,17 @@ function add() {
   todo.push(task);
   localStorage.setItem("list", JSON.stringify(todo));
 
-  
+  display();
+  document.getElementById("input").value = "";
+  return false;
 }
 
+function remove() {
+  var id = this.getAttribute('id');
+  var todo = addItems();
+  todo.splice(id, 1);
+  localStorage.setItem("list", JSON.stringify(todo));
 
-//var list = document.createElement("li");
-function addItem(text) {
-  var inputValue = document.getElementById("input").value;
-  const todo = {
-    inputValue,
-    priority: false,
-  };
-  list.push(todo);
-}
-
-if (inputValue !== "" ) {
-  // document.getElementById("list").append(todo);
-  addItem(inputValue);
-  let inputValue = "";
-  input.focus();
-}
-
-/*
-var input = document.getElementById("input");
-var temp = document.createTextNode(valueInput);
-*/
-/*   if (input) {
-    input.addEventListener("keyup", function(event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        addItem();
-      }
-    });
-  }
-  */
-
-    var span = document.createElement("SPAN");
-    var text = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.append(text);
-
-
-
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-var lis = document.querySelector("ul");
-if (lis) {
-  lis.addEventListener("click", function(event) {
-    if (event.target.tagName === "LI") {
-      event.target.classList.toggle("checked");
-    }
-  }, false);
-
+  display();
+  return false;
 }
